d3499bad469a9432e9bd22827dad707668c264ab
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-10 20:28:01 -0300

    Adicionando componente Tip.

--- advinhe/src/App.tsx
+++ advinhe/src/App.tsx
@@ -1,11 +1,13 @@
 import "./global.css"
 
 import { BodyGame } from "./components/BodyGame/BodyGame"
+import { Tip } from "./components/Tip/Tip"
 
 export default function app(){
   return(
     <div className="app">
       <BodyGame />
+      {/* <Tip /> */}
     </div>
   )
 }
