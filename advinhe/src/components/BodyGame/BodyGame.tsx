971022d84a94508ef7b21b877ce3736c0a73ebf9
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-10 19:07:44 -0300

    Adicionando componentes do corpo do game.

---
+++ advinhe/src/components/BodyGame/BodyGame.tsx
@@ -0,0 +1,8 @@
+import BodyGameStyles from "../BodyGame/bodygame.module.css"
+import { Header } from "../Header/Header"
+
+export function BodyGame() {
+    return <div className={BodyGameStyles.bodyGame}>
+        <Header />
+    </div>
+}
